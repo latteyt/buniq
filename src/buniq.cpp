5cc8c822c5e21a322f71afb6deedd683de5a3faa
latteyt <lattetao@outlook.com>
2026-04-23 18:24:06 +0800

    fix worker thread exit check order and add join

--- src/buniq.cpp
+++ src/buniq.cpp
@@ -1,4 +1,5 @@
 
+#include <atomic>
 #include <cstddef>
 #include <emmintrin.h>
 #include <fcntl.h>
@@ -8,28 +9,18 @@
 #include <stdexcept>
 
 #include <cstdio>
+#include <thread>
 #include <unistd.h>
 
 #include "bloom_filter.hpp"
 #include <cstdlib>
 #include <cstring>
 
-#define MAX_LINE_SIZE 256
-
-void pin_to_current_cpu() {
-  int cpu_id = sched_getcpu(); // 获取当前运行 CPU
-
-  cpu_set_t cpuset;
-  CPU_ZERO(&cpuset);
-  CPU_SET(cpu_id, &cpuset);
-
-  int ret = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-  if (ret != 0) {
-    perror("pthread_setaffinity_np");
-  } else {
-    printf("Pinned to CPU %d\n", cpu_id);
-  }
-}
+constexpr size_t MAX_LINE_SIZE = 128;
+constexpr size_t SLOT_SIZE = 4096 / MAX_LINE_SIZE;
+static char slots[SLOT_SIZE][MAX_LINE_SIZE];
+static std::atomic<size_t> head{0};
+static std::atomic<size_t> tail{0};
 
 int main(int argc, char *argv[]) {
   static_assert(sizeof(block_t) == 64, "block_t must be 64 bytes");
@@ -56,24 +47,61 @@ int main(int argc, char *argv[]) {
 
   bloom_filter_t bloom_filter{cardinality, precision, seed};
 
+  std::thread worker([&] {
+    while (true) {
+      size_t t = tail.load(std::memory_order_relaxed);
+      while (t == head.load(std::memory_order_acquire))
+        _mm_pause(); // empty
+
+      if (slots[t][0] == '\0')
+        break;
+
+      char *nl = (char *)memchr(slots[t], '\n', MAX_LINE_SIZE);
+      if (!nl) [[unlikely]]
+        throw std::runtime_error("Line Too Long");
+
+      size_t len = nl - slots[t] + 1;
+      if (bloom_filter.insert_if_new(slots[t], len)) // new
+        fwrite(slots[t], 1, len, stdout);
+
+      size_t next = (t + 1) % SLOT_SIZE;
+      tail.store(next, std::memory_order_release);
+    }
+  });
+
   FILE *fp = stdin;
   if (optind < argc)
     fp = fopen(argv[optind], "r");
   if (!fp)
     throw std::runtime_error("Open File Failed");
 
-  char buf[MAX_LINE_SIZE];
-  std::memset(buf, 0, sizeof(buf));
+  while (true) {
+    size_t h = head.load(std::memory_order_relaxed);
+    size_t next = (h + 1) % SLOT_SIZE;
+    while (next == tail.load(std::memory_order_acquire))
+      _mm_pause(); // full -> busy wait
 
-  while (fgets(buf, sizeof(buf), fp)) {
-    char *nl = (char *)memchr(buf, '\n', sizeof(buf));
-    if (!nl) [[unlikely]] {
-      throw std::runtime_error("Line Too Long");
+    if (!fgets(slots[h], MAX_LINE_SIZE, fp)) {
+      slots[h][0] = '\0';
+      head.store(next, std::memory_order_release);
+      break;
     }
-    size_t len = nl - buf + 1;
-    if (bloom_filter.insert_if_new(buf, len)) // new
-      fwrite(buf, 1, len, stdout);
+    head.store(next, std::memory_order_release);
   }
+
+  // char buf[MAX_LINE_SIZE];
+  // std::memset(buf, 0, sizeof(buf));
+  //
+  // while (fgets(buf, sizeof(buf), fp)) {
+  //   char *nl = (char *)memchr(buf, '\n', sizeof(buf));
+  //   if (!nl) [[unlikely]]
+  //     throw std::runtime_error("Line Too Long");
+  //
+  //   size_t len = nl - buf + 1;
+  //   if (bloom_filter.insert_if_new(buf, len)) // new
+  //     fwrite(buf, 1, len, stdout);
+  // }
   fclose(fp);
+  worker.join();
   return 0;
 }
